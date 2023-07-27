2e0d43066671da4e18e87c381bef122071e5571a
elian sanmarti <sormento231@gmail.com>
2023-07-27 11:51:51 -0300

    agregue carrito de compras y style css

---
+++ products.js
@@ -0,0 +1,28 @@
+const productos = [
+    { 
+        id: 1,
+        nombre: "harina",
+        precio: 400,
+        img: 
+        "https://i.blogs.es/95d4c3/harina-trigo-tipos/1024_2000.jpg",
+    },
+    {
+        id: 2,
+        nombre: "galletitas",
+        precio: 200,
+        img:
+        "https://media.minutouno.com/p/26bc9cf77212dba4111b163374ab4611/adjuntos/150/imagenes/039/611/0039611456/galletitas-chip-chocolate.jpg",
+    },
+    {
+        id: 3,
+        nombre: "cerveza",
+        precio: 700,
+        img:"https://www.ceupe.com/images/easyblog_articles/3121/b2ap3_large_cerveza-artesanal.jpg", 
+    },
+    {
+        id: 4,
+        nombre: "leche",
+        precio: 320,
+        img:"https://s2.abcstatics.com/media/bienestar/2020/07/09/leche-fucha-kqoG--1248x698@abc.jpg",
+    },
+];
